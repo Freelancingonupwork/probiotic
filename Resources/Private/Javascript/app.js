b51bbed18a1f3dad719075ee730474bb2c6e8345
Sebastian Sommer <mail@sebastian-sommer.com>
2021-06-01 16:55:21 +0800

    Removed jquery and fixed bootstrap javascripts

--- Resources/Private/Javascript/app.js
+++ Resources/Private/Javascript/app.js
@@ -5,16 +5,10 @@ import "bootstrap/scss/bootstrap.scss";
 import "../scss/style.scss";
 
 // // JavaScript
-require('jquery');
-require('bootstrap/dist/js/bootstrap.bundle.min.js');
-require('./custom.js');
+import 'bootstrap';
 
 // Font Awesome 5
-// import '@fortawesome/fontawesome-free/js/fontawesome'
-// import '@fortawesome/fontawesome-free/js/solid'
-// import '@fortawesome/fontawesome-free/js/regular'
-// import '@fortawesome/fontawesome-free/js/brands'
-
-
-
-
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
